255abe1e39c52a0211822bdeeeac46f8dc4221d5
Diego Bellani <bellanidiego@gmail.com>
2025-05-20 16:26:04 +0200

    Improved batch files

--- setup_env.bat
+++ setup_env.bat
@@ -1,10 +1,13 @@
 @echo off
 
+call vcvarsall x64
+
 call config.bat
 
 set "PATH=%installdir%\Programs\LLVM\bin;%PATH%"
 set "PATH=%installdir%\Programs\Python;%PATH%"
+
 if not exist %venvdir% (python -m venv %venvdir%)
-%venvdir%\Scripts\Activate
+call %venvdir%\Scripts\Activate
 pip install --no-index --find-links "%installdir%\Programs\wheelhouse" -r projreq.txt
 pip install --no-index --find-links "%installdir%\Programs\wheelhouse" torch torchvision
