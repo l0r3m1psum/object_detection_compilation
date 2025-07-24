34e9ae1b5e5c4001bc46b79a0a8a0501a419bef8
Diego Bellani <bellanidiego@gmail.com>
2025-07-24 16:48:55 +0200

    Started working on exporting quantized model

--- python/export_resnet_model_and_weights.py
+++ python/export_resnet_model_and_weights.py
@@ -24,23 +24,47 @@ def compile(mod, gen):
 					tvm.dlight.gpu.Fallback(),
 				),
 			])(mod)
-		print(cuda_mod)
-		ex = tvm.compile(cuda_mod, target)
+		# print(cuda_mod)
+		ex = tvm.build(cuda_mod, target)
 	elif gen == 'cpu':
-		ex = tvm.compile(mod, 'llvm')
+		ex = tvm.build(mod, 'llvm')
 	else:
 		raise ValueError("bad gen argument")
 	return ex
 
+def save_parameters(mod, params, path: str) -> None:
+	params_name_without_input = mod['main'].params[1:]
+	params_dict = {
+		str(k): {
+			"dtype": v.numpy().dtype.name,
+			"shape": v.numpy().shape,
+			"data": safetensors.numpy._tobytes(v.numpy())
+		}
+		for k, v in zip(params_name_without_input, params['main'])
+	}
+	order_metadata = {str(key): str(value) for value, key in enumerate(params_name_without_input)}
+	safetensors.serialize_file(params_dict, path, metadata=order_metadata)
+
+quantized = False
 
 ssl._create_default_https_context = ssl._create_stdlib_context
 torch.hub.set_dir(os.path.expandvars('%installdir%\\Programs\\hub'))
-model = torchvision.models.resnet.resnet18(weights='DEFAULT').eval()
-safetensors.torch.save_file(model.state_dict(), 'build\\resnet18.safetensors')
+
+if quantized:
+	model = torchvision.models.quantization.resnet18(
+		weights=torchvision.models.quantization.ResNet18_QuantizedWeights.DEFAULT,
+		progress=False, quantize=True
+	).eval()
+else:
+	model = torchvision.models.resnet.resnet18(weights='DEFAULT').eval()
+
+model_name = 'quantized_resnet18' if quantized else 'resnet18'
+input_dtype = torch.int8 if quantized else torch.float32
 
 utils.make_relu_not_inplace(model)
+quant_params = utils.export_quant_dequant_params(model)
 # FIXME if batchsize is 2 this does not work???
-example_args = torch.randn(1, 3, 224, 224, dtype=torch.float32),
+example_args = torch.randn(1, 3, 224, 224).to(torch.float32),
 with torch.no_grad():
 	exported_program = torch.export.export(model, example_args)
 
@@ -51,15 +75,13 @@ mod = tvm.relax.frontend.torch.from_exported_program(exported_program, keep_para
 # breakpoint()
 
 mod, params = tvm.relax.frontend.detach_params(mod)
-print(mod)
+# print(mod)
 ex = compile(mod, 'gpu')
-print(ex.as_text())
+# print(ex.as_text())
 # print(ex.as_python())
-import collections
-res = collections.OrderedDict((str(k), {"dtype": v.numpy().dtype.name, "shape": v.numpy().shape, "data": v.numpy().tobytes()}) for k, v in zip(mod['main'].params[1:], params['main']) )
-safetensors.serialize_file(res, 'build\\resnet18.safetensors', metadata={str(key): str(value) for value, key in enumerate(mod['main'].params[1:])})
+save_parameters(mod, params, f'build\\{model_name}.safetensors')
 # breakpoint()
-# TODO: invoke after compilation...
-ex.export_library(**utils.get_export_library_args('resnet18'))
+# TODO: invoke model after compilation...
+ex.export_library(**utils.get_export_library_args(model_name))
 
 # TODO: tvm.relax.transform.BindParams to bind the weights to the model
