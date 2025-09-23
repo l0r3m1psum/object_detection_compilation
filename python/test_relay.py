7385b7971986aa58f90e7fa545ccabc3af374732
Diego Bellani <bellanidiego@gmail.com>
2025-09-23 10:50:32 +0200

    Minimal demo works on simulator

--- python/test_relay.py
+++ python/test_relay.py
@@ -43,22 +43,26 @@ remote.upload(temp.relpath("graphlib.tar"))
 lib = remote.load_module("graphlib.tar")
 m = graph_executor.create(graph, lib, ctx)
 
-categ_url = "https://github.com/uwsampl/web-data/raw/main/vta/models/"
-categ_fn = "synset.txt"
-download.download(os.path.join(categ_url, categ_fn), categ_fn)
-synset = eval(open(categ_fn).read())
+# categ_url = "https://github.com/uwsampl/web-data/raw/main/vta/models/"
+# categ_fn = "synset.txt"
+# download.download(os.path.join(categ_url, categ_fn), categ_fn)
+# synset = eval(open(categ_fn).read())
+synset = ["tench", "English springer", "cassette player", "chain saw", "church",
+	"French horn", "garbage truck", "gas pump", "golf ball", "parachute",]
 
 image_url = "https://homes.cs.washington.edu/~moreau/media/vta/cat.jpg"
-image_fn = "cat.png"
+image_fn = "cat.jpg"
 download.download(image_url, image_fn)
 
 from PIL import Image
 import numpy as np
-from matplotlib import pyplot as plt
+# from matplotlib import pyplot as plt
+# plt.ion()
 
-image = Image.open(image_fn).resize((224, 224))
+# https://share.google/images/qCdEdjEnk9q0CzHmI
+image = Image.open("chainsaw.jpg").resize((224, 224))
 # plt.imshow(image)
-# plt.show()
+# plt.show(block=True)
 image = np.array(image) - np.array([123.0, 117.0, 104.0])
 image /= np.array([58.395, 57.12, 57.375])
 image = image.transpose((2, 0, 1))
@@ -85,3 +89,25 @@ for k, v in sim_stats.items():
 	# Note that there is always one warm up run
 	# Therefore we divide the overall stats by (num * rep + 1)
 	print("\t{:<16}: {:>16}".format(k, v // (num * rep + 1)))
+
+import tvm
+tvm_output = m.get_output(0, tvm.nd.empty((env.BATCH, 10), "float32", remote.cpu(0)))
+
+for b in range(env.BATCH):
+	top_categories = np.argsort(tvm_output.numpy()[b])
+	print(top_categories[-5:])
+	break
+	# Report top-5 classification results
+	print("{} prediction for sample {}".format("resnet18_v1", b))
+	print("\t#1:", synset[top_categories[-1]])
+	print("\t#2:", synset[top_categories[-2]])
+	print("\t#3:", synset[top_categories[-3]])
+	# This just checks that one of the 3 top categories;
+	# this is by no means an accurate assessment of how quantization affects
+	# classification accuracy but is meant to catch changes to the quantization
+	# pass that would accuracy in the CI.
+	detected = False
+	for k in top_categories[-3:]:
+		if "chain saw" in synset[k]:
+			detected = True
+	if not detected: raise Exception("chainsaw not detected")
