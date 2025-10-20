c91a8141f423e32ef597df20c82ff280db8486f5
Diego Bellani <bellanidiego@gmail.com>
2025-10-20 10:27:17 +0200

    More scheduling experiments

--- python/tensorize.py
+++ python/tensorize.py
@@ -1,5 +1,6 @@
 from tvm.script import tir as T
 from tvm import tir
+from tvm import te
 
 @T.prim_func
 def mma_desc(a: T.handle, b: T.handle, c: T.handle) -> None:
@@ -63,6 +64,72 @@ def alu_intrin(a: T.handle, b: T.handle, c: T.handle) -> None:
 
 tir.TensorIntrin.register("test_alu_intrin", alu_desc, alu_intrin)
 
+def align_buffers_to_128(func: tir.PrimFunc) -> tir.PrimFunc:
+    data_alignment = 128
+    buffer_map = {}
+    buffer_map_ = {} # FIXME: this variable has a shit name.
+    for var, buffer in func.buffer_map.items():
+        buffer_map[var] = tir.decl_buffer(
+            buffer.shape, buffer.dtype, buffer.name, buffer.data,
+            buffer.strides, buffer.elem_offset, buffer.scope, data_alignment,
+            buffer.offset_factor, # buffer.buffer_type, buffer.axis_separators,
+            # buffer.span
+        )
+        buffer_map_[buffer.name] = buffer_map[var]
+    def fixup(stmt: tir.Stmt) -> tir.Stmt | None:
+        # block.reads, block.writes, block.alloc_buffers is the block signature
+        if isinstance(stmt, tir.BufferLoad):
+            return tir.BufferLoad(buffer_map_[stmt.buffer.name], stmt.indices, stmt.predicate)
+        elif isinstance(stmt, tir.BufferStore):
+            return tir.BufferStore(buffer_map_[stmt.buffer.name], stmt.value, stmt.indices, stmt.predicate)
+        elif isinstance(stmt, tir.Block):
+            writes = [
+                tir.BufferRegion(buffer_map_[write.buffer.name], write.region)
+                for write in stmt.writes
+            ]
+            reads = [
+                tir.BufferRegion(buffer_map_[read.buffer.name], read.region)
+                for read in stmt.reads
+            ]
+            res = tir.Block(stmt.iter_vars, reads, writes, stmt.name_hint,
+            stmt.body, stmt.init, stmt.alloc_buffers, stmt.match_buffers, stmt.annotations)
+            return res
+        else:
+            return None
+        return None
+    body = tir.stmt_functor.ir_transform(func.body, None, fixup,
+        ["tir.BufferStore", "tir.BufferLoad", "tir.Block"])
+    res = tir.PrimFunc(
+        func.params, body, func.ret_type, buffer_map, func.attrs, func.span
+    )
+    return res
+
+A = te.placeholder((128, 128), "float32", "A")
+B = te.placeholder((128, 128), "float32", "B")
+k = te.reduce_axis((0, 128), "k")
+C = te.compute((128, 128), lambda i, j: te.sum(A[i, k] * B[k, j], axis=k), "C")
+te_func = te.create_prim_func([A, B, C]).with_attr({"global_symbol": "mm"})
+# FIXME: for some reason this is not equivalent to the TVMScript one!
+te_func = align_buffers_to_128(te_func)
+print(te_func)
+
+try:
+    sch = tir.Schedule(te_func)
+    C = sch.get_block("C")
+    i, j, k = sch.get_loops(C)
+    sch.decompose_reduction(C, i)
+    i, j, k = sch.get_loops(sch.get_block("C_update"))
+    i0, i1 = sch.split(i, (8, 16))
+    j0, j1 = sch.split(j, (8, 16))
+    k0, k1 = sch.split(k, (8, 16))
+    sch.reorder(i0, j0, k0, i1, j1, k1)
+    i0, j0, k0, i1, j1, k1 = sch.get_loops(sch.get_block("C_update"))
+    print(sch.mod["main"].script())
+    sch.tensorize(i1, "test_mma_intrin")
+    print(sch.mod["main"].script())
+except Exception as e:
+    print(e)
+
 @T.prim_func
 def before(
         A: T.Buffer((128, 128), "float32", align=128),
@@ -71,20 +138,29 @@ def before(
     ) -> None:
     # with T.block("root")
     for i, j, k in T.grid(128, 128, 128):
-        with T.block("update"):
+        with T.block("C"):
             vi, vj, vk = T.axis.remap("SSR", [i, j, k])
             T.reads(C[vi, vj], A[vi, vk], B[vj, vk])
             T.writes(C[vi, vj])
-            # with T.init(): C[vi, vj] = T.float32(0)
+            with T.init():
+                C[vi, vj] = T.float32(0)
             C[vi, vj] += A[vi, vk] * B[vj, vk]
 
+# TODO: use Schedule.cache_read to schedule the computation to something
+# that can be tensorized to VTA
+# https://tvm.apache.org/docs/reference/api/python/tir/schedule.html#tvm.tir.schedule.Schedule.cache_read
+# https://tvm.apache.org/docs/v0.8.0/topic/vta/tutorials/optimize/matrix_multiply_opt.html#sphx-glr-topic-vta-tutorials-optimize-matrix-multiply-opt-py
+
 sch = tir.Schedule(before)
-i, j, k = sch.get_loops(sch.get_block("update"))
+C = sch.get_block("C")
+i, j, k = sch.get_loops(C)
+sch.decompose_reduction(C, i)
+i, j, k = sch.get_loops(sch.get_block("C_update"))
 i0, i1 = sch.split(i, (8, 16))
 j0, j1 = sch.split(j, (8, 16))
 k0, k1 = sch.split(k, (8, 16))
 sch.reorder(i0, j0, k0, i1, j1, k1)
-i0, j0, k0, i1, j1, k1 = sch.get_loops(sch.get_block("update"))
+i0, j0, k0, i1, j1, k1 = sch.get_loops(sch.get_block("C_update"))
 sch.tensorize(i1, "test_mma_intrin")
 print(sch.mod["main"].script())
 
