148c5d7ba8ea600e94d285ce3193c08723cadbd1
Diego Bellani <bellanidiego@gmail.com>
2026-01-20 15:21:29 +0100

    Cleanup

--- clang_wrapper.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-REM TODO: determine the cmake build type dynamically
-REM TODO: rename this to clang_vta_fsim_wrapper
-REM This is supposet to be the TVM_WIN_CC.
-clang -v -Wl,-verbose ^
--l "%cd%\submodules\tvm\build\Debug\vta_fsim" ^
--l "%cd%\submodules\tvm\build\Debug\tvm_runtime" ^
-%*
