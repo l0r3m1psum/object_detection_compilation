494b360225de1c660f3056c19ef91cb92acd1f9b
Diego Bellani <bellanidiego@gmail.com>
2026-03-10 13:17:58 +0100

    Added file to compile for ZCU104

---
+++ python/compile_workloads.py
@@ -0,0 +1,28 @@
+import shutil
+import tvm
+from tvm import tir
+
+shutil.copy("submodules/tvm-vta/config/zcu104_sample.json",
+    "submodules/tvm-vta/config/vta_config.json")
+import vtar
+shutil.copy("submodules/tvm-vta/config/fsim_sample.json",
+    "submodules/tvm-vta/config/vta_config.json")
+
+env = vtar.get_env()
+target = tvm.target.Target(env.target, host=env.target_host)
+workloads = vtar.topi.resnet18_workloads
+
+for idx, wl in enumerate(workloads):
+    if idx == 0:
+        continue
+    func = vtar.topi.sq_ioa_conv2d_NCHWnc_from_workload(
+        wl, env.BATCH, env.BLOCK_IN, env.BLOCK_OUT
+    )
+    ex = tir.build(func, target, vtar.tir.get_actual_pipeline())
+    ex.export_library(
+        "build/conv2d_%d_%dx%dx%d.tar" % (idx, wl.height, wl.width, wl.in_filter)
+    )
+
+# TODO: Export also a Relax model that can run on the remote.
+# TODO: Check that this is the correct way to export from a PC to the ZCU104 and
+# that no compilation flags are missing.
