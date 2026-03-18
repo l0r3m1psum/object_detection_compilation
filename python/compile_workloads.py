39ab72d40416ce2150f14fa56c7dc842a4f5e081
Diego Bellani <bellanidiego@gmail.com>
2026-03-18 12:45:02 +0100

    Made workflow easier to run after compilation

--- python/compile_workloads.py
+++ python/compile_workloads.py
@@ -1,4 +1,5 @@
 import shutil
+import csv
 import tvm
 from tvm import tir
 
@@ -8,6 +9,15 @@ import vtar
 shutil.copy("submodules/tvm-vta/config/fsim_sample.json",
     "submodules/tvm-vta/config/vta_config.json")
 
+def write_params_csv(func: tir.PrimFunc, name: str) -> None:
+    with open(name, 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        for i, param in enumerate(func.struct_info.params):
+            shape = [v.value for v in param.shape.values]
+            dtype = param.dtype
+            writer.writerow(shape + [dtype])
+
+
 env = vtar.get_env()
 target = tvm.target.Target(env.target, host=env.target_host)
 workloads = vtar.topi.resnet18_workloads
@@ -15,21 +25,19 @@ workloads = vtar.topi.resnet18_workloads
 for idx, wl in enumerate(workloads):
     if idx == 0:
         continue
+
     func = vtar.topi.sq_ioa_conv2d_NCHWnc_from_workload(
         wl, env.BATCH, env.BLOCK_IN, env.BLOCK_OUT
     )
     ex = tir.build(func, target, vtar.tir.get_actual_pipeline())
-    ex.export_library(
-        "build/conv2d_pc_%d_%dx%dx%d.tar" % (idx, wl.height, wl.width, wl.in_filter)
-    )
+    ex.export_library("build/resnet_conv_%d_per_axis.tar" % idx)
+    write_params_csv(func, "build/resnet_conv_%d_per_axis.csv" % idx)
+
     func = vtar.topi.sq_ioa_conv2d_NCHWnc_from_workload(
         wl, env.BATCH, env.BLOCK_IN, env.BLOCK_OUT, imm_scale=3
     )
     ex = tir.build(func, target, vtar.tir.get_actual_pipeline())
-    ex.export_library(
-        "build/conv2d_pt_%d_%dx%dx%d.tar" % (idx, wl.height, wl.width, wl.in_filter)
-    )
+    ex.export_library("build/resnet_conv_%d_per_tensor.tar" % idx)
+    write_params_csv(func, "build/resnet_conv_%d_per_tensor.csv" % idx)
 
 # TODO: Export also a Relax model that can run on the remote.
-# TODO: Check that this is the correct way to export from a PC to the ZCU104 and
-# that no compilation flags are missing.
